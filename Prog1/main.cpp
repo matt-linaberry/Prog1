592f2d13d724f6b9a356a83301b8acff280e5b7f
Matt Linaberry <matt.linaberry@we-ics.com>
2015-03-14 10:16:19 -0400

    Initial Commit

---
+++ Prog1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prog1
+//
+//  Created by Matthew Linaberry on 3/14/15.
+//  Copyright (c) 2015 Matthew Linaberry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
