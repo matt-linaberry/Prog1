5112292886689e74b49a90887c30efb6992933cb
Matt Linaberry <matt.linaberry@we-ics.com>
2015-03-14 10:40:39 -0400

    This just changes things up for a product versus a sum.

--- Prog1/main.cpp
+++ Prog1/main.cpp
@@ -5,6 +5,7 @@
 //  Created by Matthew Linaberry on 3/14/15.
 //  Copyright (c) 2015 Matthew Linaberry. All rights reserved.
 //
+// This is for exercise 1.4 -- switch to a product!
 
 #include <iostream>
 
@@ -12,8 +13,8 @@ int main() {
     std::cout << "Enter two numbers: " << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
-    std::cout << "The sum of " << v1 << " and " << v2
-    << " is " << v1 + v2 << std::endl;
+    std::cout << "The product of " << v1 << " and " << v2
+    << " is " << v1 * v2 << std::endl;
     
     return 0;
 }
